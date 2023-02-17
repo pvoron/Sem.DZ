24fa65e918e67b2b19747df1024eed11deda2eb2
Voron <pn101@yandex.ru>
2023-02-17 16:13:26 +0300

    Решение задачи 10

---
+++ DZ.Sem2/1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100 && number < 1000)
+{
+    int lastDigit = number / 10;
+    int middleDigit = lastDigit % 10;
+    Console.WriteLine(middleDigit);
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
