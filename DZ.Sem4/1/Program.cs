340fbcb2aa966a796d2a5358becfe3a7e26483f5
Voron <pn101@yandex.ru>
2023-02-27 13:27:42 +0300

    Решение задачи 25

---
+++ DZ.Sem4/1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+double A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+double B = Convert.ToInt32(Console.ReadLine());
+
+double C = Math.Pow(A, B);
+
+Console.WriteLine(C);
